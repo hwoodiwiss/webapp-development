563b5e2b818ed15f61cbff7eed628e13ca4682b9
Hugo Woodiwiss <secronz@hotmail.com>
2019-11-24 00:20:29 +0000

    Started adding more functioanlity to DB class.

--- core/database.php
+++ core/database.php
@@ -2,6 +2,10 @@
 	
 	include_once 'data.php';
 	include_once 'utils.php';
+	foreach (glob(__DIR__ . "\\..\\Model\\*.php") as $filename)
+	{
+    	include_once $filename;
+	}
 
 	class DB extends PDO
 	{
@@ -16,6 +20,43 @@
 				';dbname=' . $settings[$section]['database'];
 
 				parent::__construct($connString, $settings[$section]['username'], $settings[$section]['password']);
+		}
+
+		public function SelectAll(string $TableName)
+		{
+			$TableNameLength = strlen($TableName);
+			$typeName = substr($TableName, 0, $TableNameLength - 1);
+
+			$typeFields = get_class_vars($typeName);
+			$typeFieldsStr = "";
+			$numFields = array_count_values($typeFields);
+			for($index = 0; $index < $numFields; $index++)
+			{
+				if($index != 0)
+				{
+					$typeFieldsStr = $typeFieldsStr . ', ';
+				}
+
+				$typeFieldsStr = $typeFieldsStr . $typeFields[$index];
+			}
+
+			$stmt = $this->prepare('SELECT ' . $typeFieldsStr . 'FROM ' . $TableName);
+			
+			if (!$stmt->execute()) 
+			{
+				die('An error occured:' . $stmt->errorInfo()[2]);
+			}
+
+			$data = $stmt->fetchAll();
+
+			$numRows = array_count_values($data);
+			$output = array();
+			for($index = 0; $index < $numRows; $index++)
+			{
+				$output[$index] = $typeName($data[$index]);
+			}
+
+			return $output;
 		}
 
 	}
